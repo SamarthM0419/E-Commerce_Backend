ad8d7256bc79d62d7b8a69f1e0e75025cb5af1b0
samarth2534@gmail.com <samarth2534@gmail.com>
2025-09-05 18:33:01 +0530

    setting up of notificationsService

---
+++ src/notificationsService/src/services/notificationServices.js
@@ -0,0 +1,18 @@
+const mailTransporter = require("../config/mailConfig");
+
+const sendEmail = async (to, subject, html) => {
+  try {
+    const emailInfo = await mailTransporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+    });
+    console.log(emailInfo.messageId);
+    return emailInfo;
+  } catch (error) {
+    console.error(" Email sending failed:", error.message);
+  }
+};
+
+module.exports = sendEmail;
---
+++ src/notificationsService/src/templates/signupTemplates.js
@@ -0,0 +1,4 @@
+module.exports = (name) => `
+  <h1>Welcome, ${name} 🎉</h1>
+  <p>Thanks for signing up! We're glad to have you.</p>
+`;
