e313fb642ab0183928532f1390c39f6d0b6e29b3
samarth2534@gmail.com <samarth2534@gmail.com>
2025-09-19 18:53:32 +0530

    admin/approval/:id/accept api done

--- src/vendorService/routes/adminRoutes.js
+++ src/vendorService/routes/adminRoutes.js
@@ -23,4 +23,34 @@ adminRouter.get("/admin/requests", authMiddleware, async (req, res) => {
   }
 });
 
+adminRouter.patch(
+  "/admin/approval/:id/accept",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      if (req.user.role !== "admin") {
+        res.status(403).json({ message: "Unauthorized Access . Admin's only" });
+      }
+      const vendorId = req.params.id;
+      console.log(vendorId);
+
+      const vendorUpdate = await Vendor.findByIdAndUpdate(
+        vendorId,
+        { status: "approved" },
+        { new: true }
+      );
+
+      if (!vendorUpdate) {
+        return res.status(404).json({ message: "Vendor not present" });
+      }
+
+      res
+        .status(200)
+        .json({ message: "Vendor Updated successfully", vendorUpdate });
+    } catch (error) {
+      res.status(500).json({ message: "Server error" });
+    }
+  }
+);
+
 module.exports = adminRouter;
