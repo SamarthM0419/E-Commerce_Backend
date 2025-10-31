3743b65dce0ffca64c4f965d3538af36572546d9
samarth2534@gmail.com <samarth2534@gmail.com>
2025-10-31 19:00:23 +0530

    addProduct api done

--- src/productService/routes/userProduct.js
+++ src/productService/routes/userProduct.js
@@ -28,12 +28,70 @@ userRouter.get("/product/findById", authMiddleware, async (req, res) => {
   }
 });
 
-userRouter.post("/product/addProduct", authMiddleware, async (req, res) => {
-  try {
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error", error });
+userRouter.post(
+  "/product/addProduct",
+  authMiddleware,
+  upload.array("images"),
+
+  async (req, res) => {
+    try {
+      const { role } = req.user;
+      if (!["admin", "vendor"].includes(role)) {
+        return res.status(403).json({ message: "Unauthorized Access" });
+      }
+      console.log(req.body);
+      const {
+        product_id,
+        title,
+        product_description,
+        initial_price,
+        discount,
+        final_price,
+        mainCategory,
+        department,
+        targetGroup,
+        productType,
+        categoryPath,
+        seller_name,
+        seller_information,
+      } = req.body;
+
+      const imageUrls = [];
+      for (const file of req.files) {
+        const uploadResult = await cloudinary.uploader.upload_stream(
+          { folder: "products" },
+          (error, result) => {
+            if (error) throw error;
+            imageUrls.push(result.secure_url);
+          }
+        );
+        uploadResult.end(file.buffer);
+      }
+
+      const product = new Product({
+        product_id,
+        title,
+        product_description,
+        initial_price,
+        discount,
+        final_price,
+        mainCategory,
+        department,
+        targetGroup,
+        productType,
+        categoryPath,
+        seller_name,
+        seller_information,
+        images: imageUrls,
+      });
+
+      await product.save();
+      res.status(201).json({ message: "Product added successfully", product });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Server Error", error });
+    }
   }
-});
+);
 
 module.exports = userRouter;
